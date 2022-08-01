14834eef52fdde0fb3686808a822eb024203355e
CarlosDcomp <109424074+CarlosDcomp@users.noreply.github.com>
2022-08-01 16:12:55 -0300

    Reutilização de componentes

--- src/components/Picture.vue
+++ src/components/Picture.vue
@@ -1,8 +1,11 @@
 <template>
     <img :src="avatar" :alt="descricao">
+    <reutilizacao />
 </template>
 <script>
+    import Reutilizacao from './Reutilizacao.vue'
     export default {
+        components: { Reutilizacao },
         name: 'Picture',
         data() {
             return {
