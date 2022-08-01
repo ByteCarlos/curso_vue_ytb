84f5b2472561fd64dc0e79a2f21973e6ed1ccfe9
CarlosDcomp <109424074+CarlosDcomp@users.noreply.github.com>
2022-07-31 21:49:12 -0300

    Hierarquia de componentes

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,22 @@
     <PrimeiroComponente />
     <p>O dado de app é: {{ testando }}</p>
     <LifeCycle />
+    <Pessoa />
   </div>
 </template>
 
 <script>
   import PrimeiroComponente from './components/PrimeiroComponente.vue'
   import LifeCycle from './components/LifeCycle.vue'
+  import Pessoa from './components/Pessoa.vue'
 
   export default {
     name: 'App',
     components: {
-      PrimeiroComponente,
-      LifeCycle
-    },
+    PrimeiroComponente,
+    LifeCycle,
+    Pessoa
+},
     data() {
       return {
         testando: "Teste"
---
+++ src/components/Pessoa.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h2>Esta é a descrição da pessoa: {{ nome }}</h2>
+        <Info />
+        <Form />
+    </div>
+</template>
+<script>
+import Info from './Info.vue'
+import Form from './Form.vue'
+
+    export default {
+        components: { Info, Form },
+        name: 'Pessoa',
+        data() {
+            return {
+                nome: 'Carlos'
+            }
+        }
+    }
+</script>
